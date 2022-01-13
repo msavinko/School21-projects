734a91b25e2cb9b0974f8ea366b949b7fb5269e2
Maria Savinova <71058839+msavinko@users.noreply.github.com>
2022-01-13 17:01:03 +0300

    update 13 jan 2022

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,76 +6,37 @@
 /*   By: marlean <marlean@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 13:26:39 by marlean           #+#    #+#             */
-/*   Updated: 2021/10/25 15:59:26 by marlean          ###   ########.fr       */
+/*   Updated: 2022/01/13 16:59:51 by marlean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_intlen(int n)
+char *ft_itoa(unsigned long int num)
 {
-	int	i;
+	char *str = NULL;
+	unsigned long int tmp = num;
+	int len = 0;
 
-	i = 0;
-	if (n == 0)
-		return (1);
-	while (n > 0)
+	if (num == 0)
+		len++;
+	while (tmp > 0)
 	{
-		n = n / 10;
-		i++;
+		tmp = tmp/10;
+		len++;
 	}
-	return (i);
-}
-
-static char	*ft_flags(int min, int num, char *result)
-{
-	if (min)
-		result[10] = '8';
-	if (num < 0)
-		result[0] = '-';
-	return (result);
-}
-
-static int	ft_flagmin(int n)
-{
-	if (n == -2147483648)
-		return (1);
-	else
-		return (0);
-}
-
-static int	ft_negative(int n)
-{
-	if (n == -2147483648)
-		n = (n + 1) * (-1);
-	else
-		n = n * (-1);
-	return (n);
-}
-
-char	*ft_itoa(int n)
-{
-	char	*result;
-	int		len;
-	int		numcopy;
-
-	numcopy = n;
-	len = ft_intlen(n);
-	if (n < 0)
-	{
-		n = ft_negative(n);
-		len = ft_intlen(n) + 1;
-	}	
-	result = (char *)malloc(len + 1);
-	if (!result)
+	str = malloc(len + 1);
+	if (!str)
 		return (NULL);
-	result[len] = '\0';
+	str[len] = '\0';
 	while (len > 0)
 	{
-		result[len-- - 1] = (n % 10) + '0';
-		n /= 10;
+		if ((num % 10) > 9)
+			str[len-1] = (num % 10) + 'a' - 10;
+		else
+			str[len-1] = (num % 10) + '0';
+		num = num/10;
+		len--;
 	}
-	if (ft_flagmin(numcopy) || numcopy < 0)
-		result = ft_flags(ft_flagmin(numcopy), numcopy, result);
-	return (result);
+	return (str);
 }
