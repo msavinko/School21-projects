b27090ebe3979d7800a2bae79ca9e2316ecf565c
Maria Savinova <71058839+msavinko@users.noreply.github.com>
2022-01-27 10:26:10 +0300

    updated iota function arguments in .h file

--- libft/libft.h
+++ libft/libft.h
@@ -51,7 +51,7 @@ void	ft_putendl_fd(char *s, int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_itoa(int n);
+char	*ft_itoa(unsigned long int num);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
